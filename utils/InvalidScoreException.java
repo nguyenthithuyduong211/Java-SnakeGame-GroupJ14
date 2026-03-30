e32ed87b07d7b54e35d487c3d4a5e8667b138856
nguyenthithuyduong211 <3120225035@ued.udn.vn>
2026-03-30 15:57:27 +0700

    Chuyen model va utils vao trong src

--- utils/InvalidScoreException.java
+++
@@ -1,8 +0,0 @@
-package utils;
-
-// Tiêu chí: Tự định nghĩa ít nhất 1 Custom Exception (image_6.png)
-public class InvalidScoreException extends Exception {
-    public InvalidScoreException(String message) {
-        super(message);
-    }
-}
